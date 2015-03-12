6add0e9b0023f0f4c94c20b52c10ee296c98101d
Kyoto Kopz <kopz9999@gmail.com>
2015-03-12 00:03:04 -0400

    Fixed with tests working

--- test/e_api_client_test.rb
+++ test/e_api_client_test.rb
@@ -3,8 +3,7 @@ require 'test_helper'
 class EApiClientTest < ActiveSupport::TestCase
 
   test "check endpoint" do
-  	p "Verifying if configuration loads correctly"
-    assert_match "localhost:3003", EApiClient::ActiveClient::JSON::Base.global_base_url
+    assert_match "localhost:3003", EApiClient::ActiveClient::JSON::Configurable.global_base_url
   end
 
 end
